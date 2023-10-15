23731e17e93fefe1014a9a4cf938439bfbc46b69
Flavio F <flavio.filho@hotmail.com>
2023-10-15 14:51:39 -0300

    Adicionando opção de login com Github e estilização

--- src/app/(login-routes)/page.tsx
+++ src/app/(login-routes)/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 
-import { signIn } from "next-auth/react"
-
+import LoginButtons from '@/components/LoginButtons'
 
 export default function Home() {
   return (
-    <main className="text-center w-full mx-auto max-w-[1240px] mt-16">
-      <h1 className="text-4xl font-bold">Home Page</h1>
-      <button onClick={() => signIn('google')} className="mx-auto mt-16">Entrar com Google</button>
+    <main className='text-center w-full mx-auto max-w-[1240px] mt-16'>
+      <h1 className='text-4xl font-bold mb-3'>Home Page</h1>
+
+      <div className='flex items-center justify-center'>
+        <LoginButtons />
+      </div>
+      
     </main>
   )
 }
