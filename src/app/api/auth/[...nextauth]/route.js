405cbb1b165ff80722a04cc59f59b4ceae077cfc
Flavio F <flavio.filho@hotmail.com>
2023-10-15 15:27:51 -0300

    Adicionando auth0 provider

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next"
 import GoogleProvider from "next-auth/providers/google"
 import GitHubProvider from "next-auth/providers/github"
+import Auth0Provider from "next-auth/providers/auth0"
 
 export const authOptions = {
   providers: [
@@ -11,6 +12,11 @@ export const authOptions = {
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER
     })
   ],
   secret: process.env.SECRET,
--- src/components/LoginButtons.tsx
+++ src/components/LoginButtons.tsx
@@ -24,7 +24,7 @@ export default function GoogleButton() {
       </button>
 
       <button
-      onClick={() => signIn('github')}
+        onClick={() => signIn('github')}
         type='button'
         className='py-2 px-4 flex justify-center items-center  bg-gray-600 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg '
       >
@@ -40,6 +40,7 @@ export default function GoogleButton() {
         </svg>
         Sign in with GitHub
       </button>
+
     </div>
   )
 }
